b16eab6eb982d5a328009d514706e193a94c42ae
daniel <elderdk@DESKTOP-10UCPFB.localdomain>
2022-12-20 07:19:42 +0900

    remove slack related handlers and other non-needed files.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from translator import trans
 from lang_handler import check_lang
-from token_handler import deserialzie_token
+
 from urllib import parse
-from slack_handler import get_block
+
+import json
 
 
 def back_translate(text: str, slang: str = "ko", tlang: str = "en") -> dict:
@@ -24,20 +25,23 @@ def back_translate(text: str, slang: str = "ko", tlang: str = "en") -> dict:
 
 def handler(event, context):
     
-    tokens = deserialzie_token(parse.unquote_plus(event['body']))
-    text = tokens['text']
+    text = event['body']
     slang = 'ko'
     tlang = 'en'
     
     response = back_translate(text, slang, tlang)
-    
-    block = get_block(response)
-    print(block)
+
     return {
-        'statusCode': 200,
-        'body': block
+        "statusCode": 200,
+        "message": json.dumps(response)
     }
     
 
 if __name__ == "__main__":
-    pass
+    a = handler({"body": "이 텍스트 번역 해 주세요."}, {})
+    print(a['message'])
+
+# remove slack related functions to detach the main api from slack process
+# for slack, make separate two lambda fnctions (one to receive, another to process)
+# research into lambda asynchronous execution using http header
+
